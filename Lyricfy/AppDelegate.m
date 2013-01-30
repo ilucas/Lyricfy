2d06c693e6d43f1ce483d86aabef77bb224e79c5
Lucas <lucas@War-Machine.local>
2013-01-30 15:12:03 -0200

    Initial Commit

---
+++ Lyricfy/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  Lyricfy
+//
+//  Created by Lucas on 1/30/13.
+//  Copyright (c) 2013 Lucas. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ Lyricfy/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Lyricfy
+//
+//  Created by Lucas on 1/30/13.
+//  Copyright (c) 2013 Lucas. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
